b9651a851d88055cb767bcf683347fa4210c6da2
Seongwon20 <seangwon20@gmail.com>
2025-05-19 16:04:53 +0900

    ✨feat: UserInfo.jsx에 import 추가

---
+++ src/pages/UserInfo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ZIPPiCk from "../components/ZIPPiCk";
+import "../css/UserInfo.css";
+
+const UserInfo = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserInfo
