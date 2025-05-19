cea47471dd70d997ce1710d2118b9c2327969f54
Seongwon20 <seangwon20@gmail.com>
2025-05-19 20:26:36 +0900

    ✨feat: UserInfo 수정: STEP1이 써있는 bar와 나머지 요소들을 넣기 위한 큰 박스 설정

--- src/pages/UserInfo.jsx
+++ src/pages/UserInfo.jsx
@@ -4,9 +4,21 @@ import "../css/UserInfo.scss";
 
 const UserInfo = () => {
   return (
-    <div className="top-image">
+    <div className="top"> {/* 밑에 div 들의 대빵 설정 */}
+    <div className="top-image"> {/* ZIPPICK 이미지 */}
         <img src={ZIPPICK} alt="ZIPPICK"/>
-      
+    </div>
+    <div className="top-text"> {/* ZIPPICK 이미지 밑 글씨씨 */}
+        <h3>꼭 맞는 집을 찾기 전<br />
+        <span className="yourinfo">당신의 정보</span>가 필요해요!</h3>
+        </div>
+
+        <div className="big-box">
+            <div className="step-bar">
+                <span className="step-title">STEP 1</span>
+            </div>
+        </div>
+
     </div>
   )
 }
