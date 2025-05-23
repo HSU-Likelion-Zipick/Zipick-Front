3efb5475acd013a1ac886ce0ed4b87e8a708c7fc
daye <dbk1102@naver.com>
2025-05-23 22:24:10 +0900

    ✨ : Guide.jsx에 title 추가가

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -4,7 +4,10 @@ import "../css/Guide.scss";
 const Guide = () => {
   return (
     <div className="guide">
-      
+          <div className="title">
+              <p className='zip'>" ZIPICK "</p>
+              <span className='use'> 이렇게 이용해요!</span>
+      </div>
     </div>
   )
 }
