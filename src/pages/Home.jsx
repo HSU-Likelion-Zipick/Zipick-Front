12128755fc71fff70ae63fea3fe54f7aad9382a3
daye <dbk1102@naver.com>
2025-05-23 21:24:39 +0900

    ✨ : Home.jsx에 말풍선들 추가가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
 import "../css/Home.scss";
 import Header from "../components/Header.jsx";
+import bubble1 from "../assets/bubble1.png";
+import bubble2 from "../assets/bubble2.png";
 const Home = () => {
   return (
       <div className="home">
-          <Header/>
+      <Header />
+      <div className="bubbles">
+      <img src={bubble1} alt="bubble1" className="bubble1" />
+      <img src={bubble2} alt="bubble2" className="bubble2" />
+      </div>
     </div>
   )
 }
