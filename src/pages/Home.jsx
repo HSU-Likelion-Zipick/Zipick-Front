8cc0791e9bd4d03d90e464c8a46e73967dd522c9
daye <dbk1102@naver.com>
2025-05-23 18:37:33 +0900

    ✨ : Header.jsx에 로고 컴포넌트 생성성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,10 @@
 import React, { useState } from 'react'
-import logo from "../assets/logo.svg"
-import sublogo from "../assets/Subtract.svg"
 import "../css/Home.scss";
+import Header from "../components/Header.jsx";
 const Home = () => {
   return (
       <div className="home">
-          <div className="header">
-              <img src={sublogo} alt="logo" className="logo"></img>
-              <img src={logo} alt="logo" className="logo"></img>
-          </div>
-      
+          <Header/>
     </div>
   )
 }
