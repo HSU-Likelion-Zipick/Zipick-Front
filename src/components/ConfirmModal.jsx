a2f22c8d134e5f026880e871921fbf303a1e63f1
daye <dbk1102@naver.com>
2025-05-24 16:37:48 +0900

    🎨 : 모달창에 마스코트, 타이틀, 버튼 크기 및 배치 조정

--- src/components/ConfirmModal.jsx
+++ src/components/ConfirmModal.jsx
@@ -7,10 +7,14 @@ const ConfirmModal = ({ onYes, onNo}) => {
   return (
     <div className='modal-overlay'>
           <div className='modal-box'>
-              <img src={guidemascot} alt='guidemascot' className='guidemascot'></img>
+              <div className='content'>
+                  <img src={guidemascot} alt='guidemascot' className='guidemascot'></img>
+                  <div className='titles'>
               <p className='modal-title'><strong style={{ color: "#FF5927" }}>‘자취방’</strong>에 대한 정보가 저장되었어요!</p>
-              <p className='modal-subtitle'>고민 중인 집이 더 있나요?</p>
-            
+                      <p className='modal-subtitle'>고민 중인 집이 더 있나요?</p>
+                      </div> {/*titles div */}
+              </div>{/*content div */}
+
               <div className='modal-buttons'>
               <button className="no-button" onClick={onNo}>아니오</button>
               <button className="yes-button" onClick={onYes}>네</button>
