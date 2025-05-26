7c2232c05afa5fb2b38b2db44f4b6c840cd00601
daye <dbk1102@naver.com>
2025-05-26 17:13:21 +0900

    🐛 : LoadingModal의 scss 모듈화화

--- src/components/LoadingModal.jsx
+++ src/components/LoadingModal.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import loadingmascot from "../assets/loadingmascot.png";
-import "../css/LoadingModal.scss";
+import styles from "../css/LoadingModal.module.scss";
 
 const LoadingModal = () => {
   const [progress, setProgress] = useState(0);
@@ -20,27 +20,27 @@ const LoadingModal = () => {
   }, []);
 
   return (
-    <div className="modal-overlay">
-      <div className="modal-box">
-        <div className="loading-title">Loading</div>
+    <div className={styles["modal-overlay"]}>
+      <div className={styles["modal-box"]}>
+        <div className={styles["loading-title"]}>Loading</div>
         <img
           src={loadingmascot}
           alt="로딩마스코트"
-          className="loading-mascot"
+          className={styles["loading-mascot"]}
         />
-        <div className="loading-message">
-          <span className="highlight">' PICK씨 '</span>가<br />
+        <div className={styles["loading-message"]}>
+          <span className={styles["highlight"]}>' PICK씨 '</span>가<br />
           아기사자님의
           <br />
           집을 고민하고 있어요
           <br />
           잠시만 기다려주세요!
         </div>
-        <div className="loading-text">꼭 맞는 집을 추천해드릴게요!</div>
-        <div className="progress-wrap">
-          <div className="progress-bar">
+        <div className={styles["loading-text"]}>꼭 맞는 집을 추천해드릴게요!</div>
+        <div className={styles["progress-wrap"]}>
+          <div className={styles["progress-bar"]}>
             <div
-              className="progress-bar-inner"
+              className={styles["progress-bar-inner"]}
               style={{ width: `${progress}%` }}
             />
           </div>
