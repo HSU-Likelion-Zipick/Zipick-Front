751b651f7258e505518d15583e9bcb9223d50276
nyun-nye <2001_11_7@naver.com>
2025-05-29 00:47:58 +0900

    🐛

--- src/utils/instance.jsx
+++ src/utils/instance.jsx
@@ -1,12 +1,18 @@
 import axios from "axios";
 
-const BASE_URL = `http://3.39.136.160:8080/api/`;
+// const BASE_URL = `http://3.39.136.160:8080/api/`;
+
+const BASE_URL =
+  window.location.hostname === "localhost"
+    ? "http://3.39.136.160:8080/api"
+    : "/api";
 
 const defaultInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json; charset=UTF-8",
   }, //headers
+  withCredentials: true,
 }); // const defaultInstande = axios.create
 
 export default defaultInstance;
